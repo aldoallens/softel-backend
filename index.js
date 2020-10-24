6fc7da2e864a21938ebf6eaca6b8b1875ee20902
AldoAllens <aldoallens1@gmail.com>
2020-10-23 22:48:26 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var cors = require('cors');
+var app = express();
+app.use(cors());
+app.get('/', function (req, res) {
+  res.send({ data: Math.floor(Math.random() * (150 - 1) + 1), code: 200, error: false });
+});
+app.listen(3000, function () {
+  console.log('API inicializada en el puerto 3000');
+});
